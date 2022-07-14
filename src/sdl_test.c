1c23666e289beb87299ca1ac6a933d118879a780
Ling-Lang <kulkajens82@gmial.com>
2022-07-14 13:23:41 +0200

    Learning SDL

--- src/sdl_test.c
+++ src/sdl_test.c
@@ -1,34 +1,49 @@
-#include <SDL.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sdl_test.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dylan_u </var/spool/mail/dylan_u>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/12 10:15:29 by dylan_u           #+#    #+#             */
+/*   Updated: 2022/07/12 23:50:15 by dylan_u          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <SDL.h>	
 #include <stdio.h>
-int main(/*int argc, char *argv[]*/)
+int main()
 {
 	int i = 0;
 	SDL_Window *window = NULL;
 	SDL_Surface *surface;
 	SDL_Renderer *renderer = NULL;
 	SDL_Texture *texture;
-	int count = 4;
+	int count_tblock = 4;
 	int fallingspeed = 90;
-	SDL_Rect rect[count];
-	rect[0].x = 30;
-	rect[0].y = 0;
-	rect[0].h = 10;
-	rect[0].w = 10;
+	SDL_Rect tblock[count_tblock];
+	{
+	tblock[0].x = 150;
+	tblock[0].y = 0;
+	tblock[0].h = 10;
+	tblock[0].w = 10;
 
-	rect[1].x = 40;
-	rect[1].y = 10;
-	rect[1].h = 10;
-	rect[1].w = 10;
+	tblock[1].x = 160;
+	tblock[1].y = 10;
+	tblock[1].h = 10;
+	tblock[1].w = 10;
 	
-	rect[2].x = 40;
-	rect[2].y = 0;
-	rect[2].h = 10;
-	rect[2].w = 10;
+	tblock[2].x = 160;
+	tblock[2].y = 0;
+	tblock[2].h = 10;
+	tblock[2].w = 10;
 
-	rect[3].x = 50;
-	rect[3].y = 0;
-	rect[3].h = 10;
-	rect[3].w = 10;
+	tblock[3].x = 170;
+	tblock[3].y = 0;
+	tblock[3].h = 10;
+	tblock[3].w = 10;
+	};
+	
 
 	SDL_Init(SDL_INIT_VIDEO);
 		/*SDL_CreateWindowAndRenderer(
@@ -70,11 +85,11 @@ int main(/*int argc, char *argv[]*/)
 		SDL_RenderPresent(renderer);
 
 		SDL_SetRenderDrawColor(renderer, 0xfe, 0xfe, 0x58, 0x1);
-		SDL_RenderDrawRects(renderer, &rect[0], count);
+		SDL_RenderDrawRects(renderer, &tblock[0], count_tblock);
 		SDL_RenderPresent(renderer);
 		
 		SDL_SetRenderDrawColor(renderer, 0xfe, 0xff, 0x03, 0x1);
-		SDL_RenderFillRects(renderer, &rect[0], count);
+		SDL_RenderFillRects(renderer, &tblock[0], count_tblock);
 		SDL_RenderPresent(renderer);
 
 		// Update stuff 
@@ -85,20 +100,20 @@ int main(/*int argc, char *argv[]*/)
 		SDL_RenderClear(renderer);
 		SDL_RenderPresent(renderer);
 	*/
-		while(i < 45)
+		while(i < 94)
 		{
 			
-		rect[0].y = rect[0].y +10;
-		rect[1].y = rect[1].y +10;
-		rect[2].y = rect[2].y +10;
-		rect[3].y = rect[3].y +10;
+		tblock[0].y = tblock[0].y +10;
+		tblock[1].y = tblock[1].y +10;
+		tblock[2].y = tblock[2].y +10;
+		tblock[3].y = tblock[3].y +10;
 		SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0x1);
-		SDL_RenderDrawRects(renderer, &rect[0], count);
+		SDL_RenderDrawRects(renderer, &tblock[0], count_tblock);
 		SDL_RenderPresent(renderer);
-		SDL_Delay(10);
+		SDL_Delay(100);
 
 		SDL_SetRenderDrawColor(renderer, 0x28, 0x2a, 0x2e, 0x1);
-		SDL_RenderDrawRects(renderer, &rect[0], count);
+		SDL_RenderDrawRects(renderer, &tblock[0], count_tblock);
 		SDL_RenderPresent(renderer);
 		i++;
 		/*
